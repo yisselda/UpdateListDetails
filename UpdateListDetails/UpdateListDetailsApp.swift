05540ad53556ad896cba5eacfd63820350c96e01
Yisselda <yisselda@defmethod.io>
2021-03-10 15:44:06 -0500

    Initial Commit

---
+++ UpdateListDetails/UpdateListDetailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpdateListDetailsApp.swift
+//  UpdateListDetails
+//
+//  Created by Yisselda Rhoc on 3/10/21.
+//
+
+import SwiftUI
+
+@main
+struct UpdateListDetailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
